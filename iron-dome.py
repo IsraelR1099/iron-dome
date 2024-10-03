b3bb074c8d3bd067e4ce3df23b2426f96d30dfc7
IsraelR1099 <rifarachi10@gmail.com>
2024-10-03 14:45:18 +0200

    passing args to C and setting read threshold for disk abuse

--- iron-dome.py
+++ iron-dome.py
@@ -1,41 +1,42 @@
 import sys
 import os
 import ctypes
+from ctypes import *
 
 
-def send_args(files, folders):
+def send_args(files):
     clibrary = ctypes.CDLL("./clibnotify.so")
-    for file in files:
-        print('Checking file:', file)
-        if not os.path.exists(file):
-            print('File does not exist')
-            continue
-        clibrary.notify(file.encode('utf-8'), 0)
 
-    for folder in folders:
-        print('Checking folder:', folder)
-        if not os.path.exists(folder):
-            print('Folder does not exist')
-            continue
-        clibrary.notify(0, folder.encode('utf-8)'))
+    # Conver the Python string lsit to bytes
+    files_in_bytes = [file.encode('utf-8') for file in files]
+
+    # Create an array of char pointers
+    arr = (c_char_p * len(files_in_bytes))(*files_in_bytes)
+
+    # Pass the array to the C function
+    print(f"len of files: {len(files_in_bytes)}")
+    clibrary.notify(len(files_in_bytes), arr)
 
 
 if __name__ == '__main__':
     files = []
-    folders = []
     if os.getuid() == 0:
+        print(f"len of sys args: {len(sys.argv)}")
         if len(sys.argv) < 2:
-            folders.append("/etc")
-            folders.append("/var/log")
-            folders.append("/home")
-            send_args(files, folders)
-        for arg in sys.argv[1:]:
-            if os.path.isdir(arg):
-                folders.append(arg)
-            elif os.path.isfile(arg):
-                files.append(arg)
-            print(arg)
-        send_args(files, folders)
+            print("No arguments provided, scanning /etc, /var/log, /home")
+            files.append("/etc")
+            files.append("/var/log")
+            files.append("/home")
+            send_args(files)
+        else:
+            print("Arguments provided, scanning the provided directories")
+            for arg in sys.argv[:]:
+                if os.path.isdir(arg):
+                    files.append(arg)
+                elif os.path.isfile(arg):
+                    files.append(arg)
+                print(arg)
+            send_args(files)
     else:
         print('You need to be root to run this script')
         sys.exit(1)
--- notify.c
+++ notify.c
@@ -4,85 +4,170 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/inotify.h>
+#include <poll.h>
+#include <string.h>
+#include <signal.h>
 
 #define EVENT_SIZE  (sizeof(struct inotify_event))
 #define BUF_LEN     (1024 * (EVENT_SIZE + 16))
+#define READ_THRESHOLD 10
+volatile sig_atomic_t stop = 0;
 
-void monitor_inotify(int fd)
+static void	terminate_handler(int signum)
 {
-    char buf[BUF_LEN] __attribute__((aligned(__alignof__(struct inotify_event))));
-    int len, i;
+	printf("Terminating...\n");
+	(void)signum;
+	stop = 1;
+}
 
-    // Loop to continually read events
-	while (1)
+static void	handle_events(int fd, int *wd, int *read_count, int argc, char *argv[])
+{
+	char	buf[BUF_LEN]
+		__attribute__ ((aligned(__alignof__(struct inotify_event))));;
+	const struct inotify_event	*event;
+	ssize_t				len;
+
+	//Loop while events can be read from inotify file descriptor
+	for (;;)
 	{
-		len = read(fd, buf, BUF_LEN);
-		if (len < 0)
+		len = read(fd, buf, sizeof(buf));
+		if (len < 0 && errno != EAGAIN)
 		{
-			perror("read");
-			return;
-		}
-		i = 0;
-		while (i < len)
-		{
-			struct inotify_event *event = (struct inotify_event *) &buf[i];
-			// Check if the event has a name (for file/dir)
-			if (event->len)
+			if (errno == EINTR)
 			{
-				if (event->mask & IN_MODIFY)
-				{
-					if (event->mask & IN_ISDIR)
-						printf("The directory %s was modified.\n", event->name);
-					else
-						printf("The file %s was modified.\n", event->name);
-				}
-				if (event->mask & IN_ACCESS)
-				{
-					if (event->mask & IN_ISDIR)
-						printf("The directory %s was accessed.\n", event->name);
-					else
-						printf("The file %s was accessed.\n", event->name);
-				}
+				if (stop)
+					break ;
+				continue ;
 			}
-			i += EVENT_SIZE + event->len;
+			perror("read error");
+			exit(EXIT_FAILURE);
 		}
+		if (len <= 0)
+			break ;
+		for (char *ptr = buf; ptr < buf + len; ptr += sizeof(struct inotify_event) + event->len)
+		{
+			event = (const struct inotify_event *)ptr;
+			if (event->mask & IN_OPEN)
+				printf("File %s was opened\n", event->name);
+			if (event->mask & IN_CLOSE_NOWRITE)
+				printf("File %s was closed\n", event->name);
+			if (event->mask & IN_CLOSE_WRITE)
+				printf("File %s was written\n", event->name);
+			if (event->mask & IN_ACCESS)
+				printf("File %s was accessed\n", event->name);
+		}
+		//Print the name of the watched directory
+		for (size_t i = 1; i < (size_t)argc; i++)
+		{
+			if (wd[i] == event->wd)
+			{
+				printf("Watching %s and read %d\n", argv[i], read_count[i]);
+				read_count[i]++;
+				if (read_count[i] > READ_THRESHOLD)
+					printf("WARNING: Possible disk read abuse detected.\n");
+				break ;
+			}
+		}
+		//Print the name of the file
+		if (event->len)
+			printf("File name: %s\n", event->name);
+		//Print type of filesystem object
+		if (event->mask & IN_ISDIR)
+			printf("The object is a directory.\n");
+		else
+			printf("The object is a file.\n");
 	}
 }
 
-int notify(char *files, char *folders)
+int	notify(int argc, char **argv)
 {
-	int	fd;
-	int	wd_file;
+	char			buf;
+	int			fd;
+	int			poll_num;
+	int			i;
+	int			*wd;
+	int			*read_count;
+	nfds_t			nfds;
+	struct	pollfd		fds[2];
+	struct sigaction	sa;
+	struct sigaction	so;
 
-    fd = inotify_init();
-    if (fd < 0) {
-        perror("inotify_init");
-        return -1;
-    }
+	if (argc < 2) {
+		printf("Usage: %s <file1> <file2> ... <fileN>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	fd = inotify_init();
+	if (fd < 0)
+	{
+		perror("inotify_init");
+		exit(EXIT_FAILURE);
+	}
+	//Allocate memory for watch descriptors
+	wd = (int *)calloc(argc, sizeof(int));
+	read_count = (int *)calloc(argc, sizeof(int));
+	if (wd == NULL || read_count == NULL)
+	{
+		perror("calloc");
+		exit(EXIT_FAILURE);
+	}
+	for (i = 1; i < argc; i++)
+	{
+	wd[i] = inotify_add_watch(fd, argv[i], IN_MODIFY | IN_CLOSE | IN_OPEN | IN_ACCESS);
+		if (wd[i] < 0)
+		{
+			perror("inotify_add_watch");
+			exit(EXIT_FAILURE);
+		}
+	}
+	//Prepare for polling
+	nfds = 2;
+	fds[0].fd = STDIN_FILENO;
+	fds[0].events = POLLIN;
+	fds[1].fd = fd;
+	fds[1].events = POLLIN;
 
-    // Add watch for files
-    wd_file = inotify_add_watch(fd, files, IN_MODIFY | IN_ACCESS);
-    if (wd_file < 0) {
-        perror("inotify_add_watch file");
-        return -1;
-    }
-
-/*    // Add watch for folders
-    wd_folder = inotify_add_watch(fd, folders, IN_MODIFY | IN_ACCESS);
-    if (wd_folder < 0) {
-        perror("inotify_add_watch folder");
-        return -1;
-    }*/
-
-    printf("Monitoring %s and %s for changes...\n", files, folders);
-
-    // Start monitoring
-    monitor_inotify(fd);
-
-    // Clean up
-    inotify_rm_watch(fd, wd_file);
-//    inotify_rm_watch(fd, wd_folder);
-    close(fd);
-
-    return 0;
+	//Set up signal handler
+	sa.sa_handler = terminate_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGINT, NULL, &so);
+	if (so.sa_handler != SIG_IGN)
+		sigaction(SIGINT, &sa, NULL);
+	//Wait for events and/or terminal input
+	printf("Listening for events.\n");
+	while (!stop)
+	{
+		poll_num = poll(fds, nfds, -1);
+		if (poll_num < 0)
+		{
+			if (errno == EINTR)
+				continue ;
+			perror("poll");
+			exit(EXIT_FAILURE);
+		}
+		if (poll_num > 0)
+		{
+			if (fds[0].revents & POLLIN)
+			{
+				//Console input is available. Empty stdin and quit
+				while (read(STDIN_FILENO, &buf, 1) > 0 && buf != '\n')
+					continue ;
+				break ;
+			}
+			if (fds[1].revents & POLLIN)
+			{
+				//Inotify events are available
+				handle_events(fd, wd, read_count, argc, argv);
+			}
+		}
+	}
+	printf("Listening for events stopped.\n");
+	//Clean up
+	for (i = 1; i < argc; i++)
+		inotify_rm_watch(fd, wd[i]);
+	close(fd);
+	free(wd);
+	free(read_count);
+	printf("Resources cleaned up. Exiting.\n");
+	return (0);
 }
